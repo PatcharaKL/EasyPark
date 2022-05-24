f94eea27ed2438034faaa545a4ab57d8bcc9111b
Patchara Kleebbua <patchara.pck@gmail.com>
2022-05-25 04:03:02 +0700

    Post project

--- client/pages/History.js
+++ client/pages/History.js
@@ -1,4 +1,4 @@
-import axios, { Axios } from 'axios';
+import axios, {Axios} from 'axios';
 import React, {useState, useEffect, useContext} from 'react';
 import {
   StyleSheet,
@@ -16,43 +16,60 @@ import {userContext} from '../context/userContext';
 
 export default function History() {
   const [data, setData] = useState([]);
+  const [building, setBuilding] = useState([]);
   const {user, setUser} = useContext(userContext);
   const [isDel, setIsDel] = useState(false);
   useEffect(() => {
     loadData();
   }, [isDel]);
+  const loadSingleBuilding = async req =>{
+    console.log(building)
+    const res = await axios.get('http://192.168.1.39:5000/GetAHistory', {
+      params: {
+        cusID: user[0].userID,
+        BuildID: req.building_id,
+      },
+    });
+    setData(res.data);
+  }
   const loadData = async () => {
-    const res = await axios.get('http://192.168.1.39:5000/GetHistory', {
+    const res = await axios.get('http://192.168.1.39:5000/GetAllHistory', {
       params: {
         cusID: user[0].userID,
       },
     });
     setData(res.data);
+    const resB = await axios.get(
+      'http://192.168.1.39:5000/GetBuildingHistory',
+      {
+        params: {
+          cusID: user[0].userID,
+        },
+      },
+    );
+    setBuilding(resB.data);
   };
   const onPressHandler = () => {
-    Alert.alert(
-      "Delete",
-      "Are you sure want to delete all the history?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", onPress: deleteHistory }
-      ]
-    );
-  }
+    Alert.alert('Delete', 'Are you sure want to delete all the history?', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: deleteHistory},
+    ]);
+  };
   const deleteHistory = async () => {
-    console.log('Im here')
-    const res = await axios.delete('http://192.168.1.39:5000/deleteHistory',{
-      data:{
-        cusID: user[0].userID,
-      }
-    })
-    .catch(err => console.log(err))
+    console.log('Im here');
+    const res = await axios
+      .delete('http://192.168.1.39:5000/deleteHistory', {
+        data: {
+          cusID: user[0].userID,
+        },
+      })
+      .catch(err => console.log(err));
     setIsDel(!isDel);
-  }
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text
@@ -87,7 +104,29 @@ export default function History() {
             </Text>
           </TouchableOpacity>
         </View>
-        <View style={{alignItems: 'center'}}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <FlatList
+            data={building}
+            horizontal
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: 'space-evenly',
+            }}
+            renderItem={({item}) => (
+              <View>
+                {console.log(item.Bname)}
+                <TouchableOpacity onPress={loadSingleBuilding.bind(this, item)}>
+                  <Text>{item.Bname}</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            keyExtractor={(i, index) => index.toString()}
+          />
+          <TouchableOpacity onPress={loadData}>
+            <Text>ALL</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{alignItems: 'center', flex: 10}}>
           <FlatList
             data={data}
             renderItem={({item}) => (
@@ -143,7 +182,7 @@ export default function History() {
                               {item.Bname}
                             </Text>
                             <Text style={{color: '#888888', fontSize: 15}}>
-                              {item.name} space {item.space_type_id}
+                              {item.name} space {item.space_id}
                             </Text>
                           </View>
                         </View>
--- server/app.js
+++ server/app.js
@@ -1,190 +1,228 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql2');
-const moment = require('moment');
-var session = require('express-session');
-var expressValidator = require('express-validator');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
+const moment = require("moment");
 
 const db = mysql.createPool({
-    host: 'innopark-easypark.cng6un8oxcnc.ap-southeast-1.rds.amazonaws.com',
-    user: 'admin',
-    password: '123456789',
-    database: 'easyparkdb'
+  host: "innopark-easypark.cng6un8oxcnc.ap-southeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "123456789",
+  database: "easyparkdb",
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/getAFreeSpace',(req,res)=>{
-    const typeID = req.query.typeID;
-    const inFloor = req.query.inFloor;
-    const getFreeSpace = "call getAFreeSpace(?,?)";
-    db.query(getFreeSpace,[typeID, inFloor] , (err, result)=>{
-        res.send(result[0]);
-    })
+app.get("/getAFreeSpace", (req, res) => {
+  const typeID = req.query.typeID;
+  const inFloor = req.query.inFloor;
+  const getFreeSpace = "call getAFreeSpace(?,?)";
+  db.query(getFreeSpace, [typeID, inFloor], (err, result) => {
+    res.send(result[0]);
+  });
 });
-app.post('/Parking', (req, res)=>{
-    
-})
-;
-app.get('/getSpaceType',(req, res)=>{
-    const getSpaceType = "call getSpaceTypes()"
-    db.query(getSpaceType, (err, result)=>{
-        res.send(result[0]);
-    });
+app.get("/getSpaceType", (req, res) => {
+  const getSpaceType = "call getSpaceTypes()";
+  db.query(getSpaceType, (err, result) => {
+    res.send(result[0]);
+  });
 });
 
-app.get('/getFloor',(req, res)=>{
-    const type_id = req.query.type;
-    const getFloor = "call getFloor(?)"
-    db.query(getFloor, [type_id], (err, result)=>{
-        res.send(result[0]);
-    })
+app.get("/getFloor", (req, res) => {
+  const type_id = req.query.type;
+  const getFloor = "call getFloor(?)";
+  db.query(getFloor, [type_id], (err, result) => {
+    res.send(result[0]);
+  });
 });
 
-app.get('/login',(req,res)=>{
-    const username = req.query.username;
-    const password = req.query.password;
-    const login = "call login(?,?)"
-    db.query(login, [username,password], (err, result)=>{
-        if(err){
-            console.log(err)
-        }
-        if(result[0].length > 0){
-            res.send(result[0]);
-        }
-        else{
-            console.log('Wrong Email or Password');
+app.get("/login", (req, res) => {
+  const username = req.query.username;
+  const password = req.query.password;
+  const login = "call login(?,?)";
+  db.query(login, [username, password], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result[0].length > 0) {
+      res.send(result[0]);
+    } else {
+      console.log("Wrong Email or Password");
+      res.send(false);
+    }
+  });
+});
+app.post("/register", (req, res) => {
+  const userName = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const register = "call register(?,?,?)";
+  db.query(register, [userName, email, password], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result[0]);
+    }
+  });
+});
+app.put("/updateSpaceAvailablelity", (req, res) => {
+  const spaceID = req.body.spaceID;
+  const available = req.body.available;
+  const update = "call updateSpaceAvailablelity(?,?)";
+  db.query(update, [spaceID, available], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result[0]);
+    }
+  });
+});
+app.post("/postParking", (req, res) => {
+  const INparking_time = req.body.INparking_time;
+  const INtotal_price = req.body.INtotal_price;
+  const INcheck_in = req.body.INcheck_in;
+  const INcheckout = req.body.INcheckout;
+  const INspace_id = req.body.INspace_id;
+  const INcustomerID = req.body.INcustomerID;
+  const update = "call postParking(?,?,?,?,?,?)";
+  db.query(
+    update,
+    [
+      INparking_time,
+      INtotal_price,
+      INcheck_in,
+      INcheckout,
+      INspace_id,
+      INcustomerID,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result[0]);
+      }
+    }
+  );
+});
+app.put("/Payment", (req, res) => {
+  const inPin = req.body.pin;
+  const inUser = req.body.userID;
+  const inPrice = req.body.price;
+  const spaceID = req.body.spaceID;
+  const check = "select VirtualPoint from credit_card where customerID = (?)";
+  const pay = "call Payment(?,?,?)";
+  const update = "call updateSpaceAvailablelity(?,?)"
+  db.query(check, [inUser], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+        console.log(result[0].VirtualPoint)
+      if (result[0].VirtualPoint > inPrice) {
+        db.query(pay, [inPin, inUser, inPrice], (err, result) => {
+          if (err) {
+            console.log(err);
             res.send(false);
-        }
-    })
-})
-app.post('/register',(req, res)=>{
-    const userName = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const register = "call register(?,?,?)"
-    db.query(register, [userName,email,password], (err, result)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.send(result[0]);   
-        }
-    })
-});
-app.put('/updateSpaceAvailablelity', (req,res) =>{
-    const spaceID = req.body.spaceID;
-    const available = req.body.available;
-    const update = "call updateSpaceAvailablelity(?,?)"
-    db.query(update, [spaceID,available], (err, result)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(result[0]);
-        }
-    })
-})
-app.post('/postParking',(req, res)=>{
-    const INparking_time = req.body.INparking_time;
-    const INtotal_price = req.body.INtotal_price;
-    const INcheck_in = req.body.INcheck_in;
-    const INcheckout = req.body.INcheckout;
-    const INspace_id = req.body.INspace_id;
-    const INcustomerID = req.body.INcustomerID;
-    const update = "call postParking(?,?,?,?,?,?)"
-    db.query(update, [INparking_time,INtotal_price,INcheck_in,INcheckout,INspace_id,INcustomerID], (err, result)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(result[0]);
-        }
-    })
-})
-app.put('/Payment', (req, res) =>{
-    const inPin = req.body.pin;
-    const inUser = req.body.userID;
-    const inPrice = req.body.price;
-    const spaceID = req.body.spaceID;
-    const update = "call updateSpaceAvailablelity(?,?)"
-    const pay = "call Payment(?,?,?)"
-    db.query(pay, [inPin,inUser,inPrice], (err, result)=>{
-        if(err){
-            console.log(err)
-            res.send(false)
-        }
-        else{
-            if(result.affectedRows > 0){
-                db.query(update, [spaceID,'Y'], (err, result)=>{
-                    if(err){
-                        console.log(err);
-                    }
-                    else{
-                        res.send(result[0]);
-                    }
-                })
-                res.send(true);
-            }else {
-                res.send(false);
+          } else {
+            if (result.affectedRows > 0) {
+              db.query(update, [spaceID, "Y"], (err, result) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  res.send(result[0]);
+                }
+              });
+              res.send(true);
+            } else {
+              res.send(false);
             }
-        }
-    })
-})
-app.post('/CreateCustomer',(req, res) => {
-    const customerID = req.body.customerID;
-    const Fname = req.body.Fname;
-    const Lname = req.body.Lname;
-    const tel = req.body.tel;
-    const id_card_no = req.body.id_card_no;
-    const userID = req.body.customerID;
-    console.log(customerID)
-    console.log(Fname)
-    console.log(Lname)
-    console.log(tel)
-    console.log(id_card_no)
-    console.log(userID)
-    const post = "call CreateCustomer(?,?,?,?,?,?)"
-    db.query(post, [customerID,Fname,Lname,tel,id_card_no,userID], (err, result)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(result[0]);
-        }
-    })
-})
-app.get('/GetHistory',(req,res)=>{
+          }
+        });
+      } else {
+        res.send(false);
+      }
+    }
+  });
+});
+app.post("/CreateCustomer", (req, res) => {
+  const customerID = req.body.customerID;
+  const Fname = req.body.Fname;
+  const Lname = req.body.Lname;
+  const tel = req.body.tel;
+  const id_card_no = req.body.id_card_no;
+  const userID = req.body.customerID;
+  console.log(customerID);
+  console.log(Fname);
+  console.log(Lname);
+  console.log(tel);
+  console.log(id_card_no);
+  console.log(userID);
+  const post = "call CreateCustomer(?,?,?,?,?,?)";
+  db.query(
+    post,
+    [customerID, Fname, Lname, tel, id_card_no, userID],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result[0]);
+      }
+    }
+  );
+});
+app.get("/GetAllHistory", (req, res) => {
+  const cusID = req.query.cusID;
+  const getHis = "call GetAllHistory(?)";
+  db.query(getHis, [cusID], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+      res.send(result[0]);
+    }
+  });
+});
+app.get("/GetAHistory", (req, res) => {
     const cusID = req.query.cusID;
-    const getHis = "call GetHistory(?)"
-    db.query(getHis, [cusID], (err, result)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log(result)
-            res.send(result[0]);
-        }
-    })
-})
-app.delete('/deleteHistory',(req,res) => {
-    const cusID = req.body.cusID
-    const delHis = "call deleteHistory(?)"
-    console.log(cusID)
-    db.query(delHis, [cusID], (err, result)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log(result)
-            res.send(result[0]);
-        }
-    })
-})
-app.listen(5000, ()=>{
-    console.log("listening to port 5000")
+    const BuildID = req.query.BuildID;
+    console.log(BuildID)
+    const getHis = "call GetAHistory(?,?)";
+    db.query(getHis, [cusID,BuildID], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+        res.send(result[0]);
+      }
+    });
+  });
+  app.get("/GetBuildingHistory", (req, res) => {
+    const cusID = req.query.cusID;
+    const getHis = "call GetBuildingHistory(?)";
+    db.query(getHis, [cusID], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+        res.send(result[0]);
+      }
+    });
+  });
+app.delete("/deleteHistory", (req, res) => {
+  const cusID = req.body.cusID;
+  const delHis = "call deleteHistory(?)";
+  console.log(cusID);
+  db.query(delHis, [cusID], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+      res.send(result[0]);
+    }
+  });
+});
+app.listen(5000, () => {
+  console.log("listening to port 5000");
 });
